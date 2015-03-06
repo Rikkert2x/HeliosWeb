9aac7d8730839122d5aa7be1a21525441a509df2
RickdeM <rickdeman1991@gmail.com>
2015-03-06 14:43:19 +0100

    Update index.php
    Replaced double spaces for Tabs

--- index.php
+++ index.php
@@ -8,55 +8,55 @@
 	
 ?>
 
-  <!-- Page Content -->
-  <div class="container">
+	<!-- Page Content -->
+	<div class="container">
 
-    <!-- Page Heading -->
-    <div class="row" style="margin-top:35px;">
-      <div class="col-lg-12">
-        <h1 class="page-header">Page Heading
-          <small>Secondary Text</small>
-        </h1>
-      </div>
-    </div>
-    <!-- /.row -->
+		<!-- Page Heading -->
+		<div class="row" style="margin-top:35px;">
+			<div class="col-lg-12">
+				<h1 class="page-header">Page Heading
+					<small>Secondary Text</small>
+				</h1>
+			</div>
+		</div>
+		<!-- /.row -->
 
-    <!-- Projects Row -->
-    <div class="row">
-      <div class="col-md-3 portfolio-item">
-        <a href="ART">
-          <img class="img-responsive" src="assets/img/400x300_ART.png" data="http://placehold.it/400x300/.png/&text=ART" alt="">
-        </a>
-      </div>
-      <div class="col-md-3 portfolio-item">
-        <a href="GRV">
-          <img class="img-responsive" src="assets/img/400x300_GRV.png" alt="http://placehold.it/400x300/.png/&text=GRV">
-        </a>
-      </div>
-      <div class="col-md-3 portfolio-item">
-        <a href="ART_TREE">
-          <img class="img-responsive" src="assets/img/400x300_ART_TREE.png" alt="http://placehold.it/400x300/.png/&text=ART%20TREE">
-        </a>
-      </div>
-    </div>
-    <!-- /.row -->
+		<!-- Projects Row -->
+		<div class="row">
+			<div class="col-md-3 portfolio-item">
+				<a href="ART">
+					<img class="img-responsive" src="assets/img/400x300_ART.png" data="http://placehold.it/400x300/.png/&text=ART" alt="">
+				</a>
+			</div>
+			<div class="col-md-3 portfolio-item">
+				<a href="GRV">
+					<img class="img-responsive" src="assets/img/400x300_GRV.png" alt="http://placehold.it/400x300/.png/&text=GRV">
+				</a>
+			</div>
+			<div class="col-md-3 portfolio-item">
+				<a href="ART_TREE">
+					<img class="img-responsive" src="assets/img/400x300_ART_TREE.png" alt="http://placehold.it/400x300/.png/&text=ART%20TREE">
+				</a>
+			</div>
+		</div>
+		<!-- /.row -->
 		
 <?php if( $Member === true ){ ?>
-    <!-- Projects Row -->
-    <div class="row">
-      <div class="col-md-3 portfolio-item">
-        <a href="ART">
-          <img class="img-responsive" src="assets/img/PrijsUpdate.png" alt="">
-        </a>
-      </div>
-    </div>
-    <!-- /.row -->
+		<!-- Projects Row -->
+		<div class="row">
+			<div class="col-md-3 portfolio-item">
+				<a href="ART">
+					<img class="img-responsive" src="assets/img/PrijsUpdate.png" alt="">
+				</a>
+			</div>
+		</div>
+		<!-- /.row -->
 
 <?php } ?>    
-    <hr>
+		<hr>
 
-  </div>
-  <!-- /.container -->
-  
+	</div>
+	<!-- /.container -->
+	
 </body>
 </html>
